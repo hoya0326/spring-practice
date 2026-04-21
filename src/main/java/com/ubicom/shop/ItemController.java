808ca6d69fb2b360660833cb6affff3e5999f99c
hoya0326 <rbal5430@naver.com>
2026-04-21 12:37:18 +0900

    desktop1

--- src/main/java/com/ubicom/shop/ItemController.java
+++ src/main/java/com/ubicom/shop/ItemController.java
@@ -1,8 +1,15 @@
 package com.ubicom.shop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
-
+    @GetMapping("/list")
+    String list(Model model)
+    {
+        model.addAttribute("name", "비싼바지");
+        return "list.html";
+    }
 }
---
+++ src/main/java/com/ubicom/shop/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.ubicom.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<> {
+
+}
